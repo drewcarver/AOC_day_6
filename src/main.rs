871157b8571e0d6b9730fc2d8056ecf776a281c7
Andrew Carver <drew.carver@outlook.com>
2023-02-10 21:27:54 -0500

    fix take_while

--- src/main.rs
+++ src/main.rs
@@ -6,31 +6,34 @@ fn is_unique(input: String) -> bool {
     chars.dedup();
 
     let result = chars.len() == input.len();
-    println!("input is: {}", input); 
-    println!("result is: {}", result); 
 
     result
 }
 
-fn find_start_of_transmission(input: String) -> Result<usize, String> {
+fn find_start_of_transmission(window_size: usize, input: String) -> Result<usize, String> {
     let transmission = input.chars().collect::<Vec<char>>();
-    println!("{}", transmission.len());
-    let mut end_range_iter = (4..transmission.len())
-        .into_iter()
-        .map(|i| {
-            (i-4, i)
-        })
-        .take_while(|i| !is_unique(transmission[i.0..i.1].iter().collect::<String>()));
 
-
-   Ok(end_range_iter.next().unwrap().1)
+    (window_size..transmission.len())
+    .map(|i| (i-window_size, i))
+    .take_while(|i| !is_unique(transmission[i.0..i.1].iter().collect::<String>()))
+    .collect::<Vec<_>>()
+    .last()
+    .map(|indices| indices.1 + 1)
+    .ok_or("Unable to find a non-duplicate".to_string())
 }
 
 fn main() {
-    let input = fs::read_to_string("input.txt")
-        .map_err(|_| "Unable to read file".to_string());
+    let is_problem_one = true;
+    let window_size = if is_problem_one { 4 } else { 14 };
+    let result = fs::read_to_string("input.txt")
+        .map_err(|_| "Unable to read file".to_string())
+        .and_then(|input| find_start_of_transmission(window_size, input));
 
-    println!("Hello, world!");
+    match result {
+        Ok(r) => println!("Beginning is: {}", r),
+        Err(e) => println!("{}", e)
+    }
+    
 }
 
 #[cfg(test)]
@@ -41,6 +44,6 @@ mod tests {
     fn should_find_first_four_non_duplicates() {
         let input = "mjqjpqmgbljsphdztnvjfqwrcgsmlb".to_string();
 
-        assert_eq!(Ok(7), find_start_of_transmission(input));
+        assert_eq!(Ok(7), find_start_of_transmission(4, input));
     }
 }
